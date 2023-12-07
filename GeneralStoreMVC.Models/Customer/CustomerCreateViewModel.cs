5162f7ed1bb88c26219d8fff46fbb96c18e9ca38
Sam Page <samnpage96@gmail.com>
2023-12-06 19:36:27 -0500

    fixed typo in email property

--- GeneralStoreMVC.Models/Customer/CustomerCreateViewModel.cs
+++ GeneralStoreMVC.Models/Customer/CustomerCreateViewModel.cs
@@ -2,5 +2,5 @@ namespace GeneralStoreMVC.Models.Customer;
 public class CustomerCreateViewModel
 {
     public string Name { get; set; } = string.Empty;
-    public string Emaiil { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
 }
