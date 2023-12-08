33ba2a0b497c08017595bbef88907d8151cd65e2
Sam Page <samnpage96@gmail.com>
2023-12-07 21:37:46 -0500

    create method updated

--- GeneralStoreMVC.MVC/Controllers/TransactionController.cs
+++ GeneralStoreMVC.MVC/Controllers/TransactionController.cs
@@ -1,5 +1,6 @@
 using GeneralStoreMVC.Data;
 using GeneralStoreMVC.Models.Transaction;
+using GeneralStoreMVC.Services.Product;
 using GeneralStoreMVC.Services.Transaction;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -10,24 +11,25 @@ public class TransactionController : Controller
 {
     private readonly ITransactionService _transactionService;
     private readonly GeneralStoreDbContext _ctx;
+    private readonly IProductService _productService;
 
-    public TransactionController(ITransactionService transactionService, GeneralStoreDbContext dbContext)
+    public TransactionController(ITransactionService transactionService, GeneralStoreDbContext dbContext, IProductService productService)
     {
         _transactionService = transactionService;
         _ctx = dbContext;
+        _productService = productService;
     }
 
     public async Task<IActionResult> Create(int customerId)
     {
-        var productList = await _ctx.Products
-            .Select(p => new SelectListItem
-            {
-                Value = p.Id.ToString(),
-                Text = $"{p.Name} ({p.Price:C})",
-            })
-            .ToListAsync();
+        var products = await _productService.GetAllProductsAsync();
+        List<SelectListItem> productSelectList = new();
+        foreach (var product in products)
+        {
+            productSelectList.Add(new SelectListItem{Value = product.Id.ToString(), Text = $"{product.Name}"});
+        }
 
-        ViewData["Products"] = productList;
+        ViewData["Products"] = productSelectList;
 
         return View(await _transactionService.GetCreateTransactionAsync(customerId));
     }
