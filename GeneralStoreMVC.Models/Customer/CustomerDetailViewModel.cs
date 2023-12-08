31d6ad3666f6b03f2b1c309b26cfd994acd602d3
Sam Page <samnpage96@gmail.com>
2023-12-08 17:18:34 -0500

    added transactions list

--- GeneralStoreMVC.Models/Customer/CustomerDetailViewModel.cs
+++ GeneralStoreMVC.Models/Customer/CustomerDetailViewModel.cs
@@ -1,7 +1,11 @@
+using GeneralStoreMVC.Models.Transaction;
+
 namespace GeneralStoreMVC.Models.Customer;
 public class CustomerDetailViewModel
 {
     public int Id { get; set; }
     public string? Name { get; set; }
     public string? Email { get; set; }
+
+    public List<TransactionListItem> Transactions { get; set; } = new();
 }
